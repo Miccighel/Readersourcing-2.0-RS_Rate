0aa85a01a3a9530adea93b33f2719736fdcd7dae
Miccighel <miccighel@gmail.com>
2018-09-17 14:50:20 +0200

    Fixes to the previous commit

---
+++ src/js/background.js
@@ -0,0 +1,16 @@
+////////// PAGE STATE MATCHING /////////
+
+chrome.runtime.onInstalled.addListener(function () {
+    chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
+        chrome.declarativeContent.onPageChanged.addRules([{
+            conditions: [new chrome.declarativeContent.PageStateMatcher({
+                pageUrl: {
+                    urlContains: 'pdf'
+                },
+            })
+            ],
+            actions: [new chrome.declarativeContent.ShowPageAction()]
+        }]);
+    });
+
+});
