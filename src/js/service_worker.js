1ad7df51422041b529897ce6a318ceb8efc7cf55
Michael Soprano <miccighel@gmail.com>
2023-12-21 13:27:18 +0100

    Manifest Upgrade: Transition to V3
    - Update default endpoint to communicate with the backend.
    - Migrate Font Awesome package from CDN to npm to comply with Chrome's Content Security Policy (CSP).
    - Rename background script to "service_worker" in accordance with the updated manifest.
    - Update the corresponding src attribute within each HTML file.
    - Update .gitignore.

--- src/js/service_worker.js
+++ src/js/service_worker.js
@@ -1,7 +1,7 @@
 //######### DEFAULT STARTUP VALUES #########//
 
 chrome.runtime.onInstalled.addListener(() => {
-    let hostDefault = "https://rs-server.herokuapp.com/";
+    let hostDefault = "http://readersourcing.hrhbdyetgqdmc8d5.westeurope.azurecontainer.io:3000/";
     chrome.storage.sync.set({host: hostDefault}, () => {
         console.log("Default value for option \"Host\" set.");
         chrome.storage.sync.get(['host'], result => console.log(`Value for option "Host" is ${result.host}`));
